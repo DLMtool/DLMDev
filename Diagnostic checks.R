f1fab49f703280c9351cd1e6f596ed0d16185bb9
tcarruth <tomsinbox@gmail.com>
2017-04-25 09:33:00 -0700

    some more checks

--- Diagnostic checks.R
+++ Diagnostic checks.R
@@ -7,11 +7,14 @@ setup()
 CHECKS<-new('list')
 
 
+# --------- Implementation error model checks ------------------------------------------
 
-# --------- Implementation model checks ------------------------------------------
+# --- Output MPs -----------------------
 
 OM<-new('OM',Albacore)
+OM@seed<-runif(1)
 MPs<-c("AvC","FMSYref","DCAC")
+nMPs<-length(MPs)
 
 test1<-runMSEdev_imp(OM,MPs) # Base
 
@@ -27,11 +30,42 @@ test4<-runMSEdev_imp(OM,MPs) # 20% underage
 OM@TACSD<-c(0.4,0.5)
 test5<-runMSEdev_imp(OM,MPs) # Implementation uncertainty
 
-I1<-sum(NOAA_plot(test1)$PNOF>NOAA_plot(test2)$PNOF)==3 # Overage should be lower PNOF
-I2<-sum(NOAA_plot(test2)$PNOF>NOAA_plot(test3)$PNOF)==3 # More overage should be even lower PNOF
-I3<-sum(NOAA_plot(test4)$PNOF>NOAA_plot(test1)$PNOF)==3 # Underage should be higher PNOF
-I4<-sum(NOAA_plot(test3)$VY>NOAA_plot(test5)$VY)==3     # Variability should be lower vY metric
+I1<-sum(NOAA_plot(test1)$PNOF>NOAA_plot(test2)$PNOF)==nMPs # Overage should be lower PNOF
+I2<-sum(NOAA_plot(test2)$PNOF>NOAA_plot(test3)$PNOF)==nMPs # More overage should be even lower PNOF
+I3<-sum(NOAA_plot(test4)$PNOF>NOAA_plot(test1)$PNOF)==nMPs # Underage should be higher PNOF
+I4<-sum(NOAA_plot(test3)$VY>NOAA_plot(test5)$VY)==nMPs     # Variability should be lower VY metric
 
-CHECKS$Implementation<-data.frame(I1, I2, I3, I4)
+
+CHECKS$Imp_output<-data.frame(I1, I2, I3, I4)
+
+
+# --- Size limit MPs -------------------
+
+OM<-new('OM',Albacore)
+OM@seed<-runif(1)
+MPs<-c("matlenlim","matlenlim2")
+nMPs<-length(MPs)
+
+test1<-runMSEdev_imp(OM,MPs)
+
+OM@SizeLimFrac<-c(1.2,1.2)
+
+test2<-runMSEdev_imp(OM,MPs)
+
+I5<-sum(NOAA_plot(test1)$PNOF>NOAA_plot(test2)$PNOF)==nMPs
+
+CHECKS$Imp_size<-data.frame(I5)
+
+
+
+# --------- Reference MP checks --------------------------------------------------
+
+
+# --------- Observation model checks ---------------------------------------------
+
+
+# --------- 
+
+
 
 CHECKS$Implementation
