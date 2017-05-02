e63830bc28fe90cee9265002bd934857298a025d
tcarruth <tomsinbox@gmail.com>
2017-05-02 15:59:37 -0700

    6b and 5c polish
    6a almost finished

--- Disused code/Misc/DLM_FAO/Exercises/Module6b.r
+++ Disused code/Misc/DLM_FAO/Exercises/Module6b.r
@@ -1,31 +1,82 @@
-## Exercise 6b: Time varying parameters and ecosystem considerations	(~ 40 minutes) ####
+
+# ====================================================================================
+# === DLMtool Exercise 6b: Time-varying parameters and ecosystem considerations ======
+# ====================================================================================
+#
+# Similarly to the observation processes and fishing efficiency changes 
+# considered in excercise 6a, it may be desirable to test the robustness of 
+# MPs over a range time-varying biological parameters (ie changes in growth
+# due to climate change or recruitment regime shifts).
 
 
-## Initial set-up (only required if new R session) ####
+
+# === Setup (only required if new R session) =====================================
+
 library(DLMtool)  
-setup() 
-
-## Task 1: ####
+setup()
 
 
 
-## Tasks Below are Optional #####
+# === Task 1: run the reference MSE ==============================================
+# 
+# Here we use the yellowfin operating models to conduct a reference MSE
 
-## More Advanced ####
+MPs<-c("Ltarget1","Ltarget4","Itarget4","HDAAC","Fratio","IT5", "IT10",
+       "DCAC","DCAC4010","Islope1","MCD","ITM","TCPUE","TCPUE_e","AvC")
 
-## Most Advanced ####
+SWOref<-Swordfish_OM
+
+Ref<-runMSE(SWOref, MPs=MPs, CheckMPs=T)
+
+NOAA_plot(Ref)
+
+# Q1.1 Which MP or MPs might you consider?
 
 
 
+# === Task 2: run the robustness MSE ==============================================
+# 
+# Now lets assume that there could be some changes in growth rate due
+# to oceanographic conditions, that our view of stock level is optimistic
+# and there could be phase-shifts in recruitment we are not accounting for.
 
-## Most Advanced ####
+SWOrob<-Swordfish_OM
 
-# Task 10: Create a Stock object with cyclical recruitment patterns (see DLMtool documentation) ####
-# and plot the Stock object to show sampled recruitment deviations
+SWOrob@Kgrad<-c(0,0.5)       # a negative % annual change in growth K
+SWOrob@D<-SWOref@D/2         # current depletion could be actually half
+SWOrob@Period<-c(15,25)      # 10-15 year productivity phase shifts
+SWOrob@Amplitude<-c(0.1,0.3) # 10% - 30% changes in recruitment
 
-# Questions:
-# a) Do phase shifts in recruitment impact the performance of the MPs similarly?
+Rob<-runMSE(SWOrob, MPs=MPs, CheckMPs=T)
 
-# b) 
+NOAA_plot(Rob)
+
+# Q2.1  How has absolute performance changed?
+#
+# Q2.2  How has the trade-off in NOAA_plot changed with the advent
+#       of additional operating model uncertainty?
+#
+# Q2.3  Which MPs do well regardless?
+
+
+
+# === Optional tasks ===============================================================
+# 
+# There are few other variables can can be time-varying in DLMtooloperating models including
+#
+# Mgrad: gradients in natural mortality rate (disease,fewer/more numerous predators)
+# Linfgrad: gradient in maximum length
+
+# Task 3: Add changes in Mgrad and Linfgrad
+# 
+# Q3.1    How do these affect the performance picture of the reference MSE run?
+#
+# Q3.2    What additional time-varying parameters would be relevant to stocks
+#         that you work on?
+
+
+# ==================================================================================
+# === End of Exercise 5b ===========================================================
+# ==================================================================================
 
 
